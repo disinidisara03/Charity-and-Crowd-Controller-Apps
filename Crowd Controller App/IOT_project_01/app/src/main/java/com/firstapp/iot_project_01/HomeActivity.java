41a8cc548b30fdbcd9ca46071228127e21a2638e
Disini Disara <gmddrathnayaka@students.nsbm.lk>
2021-07-03 18:55:08 +0530

    Crowd Controller App

---
+++ Crowd Controller App/IOT_project_01/app/src/main/java/com/firstapp/iot_project_01/HomeActivity.java
@@ -0,0 +1,93 @@
+package com.firstapp.iot_project_01;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class HomeActivity extends AppCompatActivity {
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+     TextView retriveTV,staus,timeperiod;
+     Button next,back;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        databaseReference = firebaseDatabase.getReference().child("Count");
+
+        retriveTV = findViewById(R.id.no_of_people);
+
+
+        timeperiod = findViewById(R.id.time);
+
+        next = (Button) findViewById(R.id.next05);
+
+        back = (Button) findViewById(R.id.back);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Sendusertostatuspage();
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                senduserback();
+            }
+        });
+
+        getdata();
+
+
+
+    }
+
+    private void senduserback() {
+        Intent back = new Intent(HomeActivity.this,HomeActivity00.class);
+        startActivity(back);
+    }
+
+    private void Sendusertostatuspage() {
+        Intent status = new Intent(HomeActivity.this, StatusActivity.class);
+        startActivity(status);
+    }
+
+
+    private void getdata() {
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+              int intval2 = Integer.valueOf(String.valueOf(snapshot.getValue()));
+
+                retriveTV.setText(snapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(HomeActivity.this, "Fail to get data.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
