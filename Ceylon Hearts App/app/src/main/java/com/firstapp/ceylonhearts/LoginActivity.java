5df45c97c61823f723b77be656079dbf77362ff3
Disini Disara <gmddrathnayaka@students.nsbm.lk>
2021-07-03 18:56:23 +0530

    Ceylon Hearts App

---
+++ Ceylon Hearts App/app/src/main/java/com/firstapp/ceylonhearts/LoginActivity.java
@@ -0,0 +1,39 @@
+package com.firstapp.ceylonhearts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextView SignUpHere;
+    EditText email;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        email = (EditText) findViewById(R.id.login_email);
+        SignUpHere = (TextView) findViewById(R.id.sign_up_txt_view);
+
+      
+        SignUpHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
